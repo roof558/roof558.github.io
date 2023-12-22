0a5effe707b6fc4de6f0f590dbcf14e12c6907e8
Justin Lee <dlee3@eecs.wsu.edu>
2023-12-21 17:18:47 -0800

    About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,30 +2,35 @@ import React from "react";
 
 const AboutMe = () => {
     return (
-        <div className="bg-purple-200 h-screen w-screen text-center" id="about">
-            <h1>
+        <div className="bg-purple-200 h-screen w-screen" id="about">
+            <h1 className="text-5xl pt-12 text-center">
                 About Me
             </h1>
-            <p>
-                Name: Dongjoon Lee
-                <br />
-                Preferred Name: Justin Lee
-                <br />
-                Graudated School: Washington State University
-                <br />
-                Degree: Computer Science
-                <br />
-                Email: roof558@gmail.com
-                <br />
-                Phone: 425-273-8798
-            </p>
-            <p>
-                Github: https://github.com/roof558?tab=repositories
-                <br />
-                Portfolio Website: https://roof558.github.io/
-                <br />
-                LinkedIn: https://www.linkedin.com/in/dongjoon-lee-a11bb6161/
-            </p>
+            <div className="flex pt-6 text-xl text-left list-none ml-72">
+                <ul className="text-left list-none">
+                    <li>Name: Dongjoon (Justin) Lee</li>
+                    <li>Graudated School: Washington State University</li>
+                    <li>Bachelor's Degree: Computer Science</li>
+                    <li>Email: roof558@gmail.com</li>
+                    <li>Phone: 425-273-8798</li>
+                    <li>Github:
+                        <a className="text-blue-700 underline ml-2" href="https://github.com/roof558?tab=repositories" target="_blank"  rel="noopener noreferrer">
+                            https://github.com/roof558?tab=repositories
+                        </a>
+                    </li>
+                    <li>Portfolio Website:
+                        <a className="text-blue-700 underline ml-2" href="https://roof558.github.io/" target="_blank" rel="noopener noreferrer">
+                            https://roof558.github.io/
+                        </a>
+                    </li>
+                    <li>LinkedIn: 
+                        <a className="text-blue-700 underline ml-2" href="https://www.linkedin.com/in/dongjoon-lee-a11bb6161/" target="_blank" rel="noopener noreferrer">
+                            https://www.linkedin.com/in/dongjoon-lee-a11bb6161/                        
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
         </div>
     );
 };
--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -3,9 +3,11 @@ import React from "react";
 const Project = () => {
     return (
         <div className="bg-purple-200 h-screen w-screen text-center" id="project">
-            <p>
+            <h1 className="text-2xl pb-1">
                 Project Page
-            </p>
+            </h1>
+
+            
         </div>
     );
 };
